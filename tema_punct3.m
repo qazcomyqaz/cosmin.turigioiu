a277ea719444c6adb37c2af86c7a07a9e70541bf
qazcomyqaz <32986730+qazcomyqaz@users.noreply.github.com>
2017-11-08 23:18:57 +0200

    Add files via upload

---
+++ tema_punct3.m
@@ -0,0 +1,26 @@
+function tema_punct3()
+    N = 4;
+    ts = 0.001;
+    t = 0:ts:N;
+    
+    k = 1;
+    
+    for i = 1:length(t)
+     if  t(i) <= k*0.25
+         if k <= length(t)
+             x1(i) = t(k);
+         else
+             x1(i) = 0;
+         end
+     else
+        if k <= length(t)
+             x1(i) = t(k);
+        end
+        k = k+1; 
+     end
+    end
+    
+    axis([0 20 -5 3])
+    plot(t,x1);
+   
+end
