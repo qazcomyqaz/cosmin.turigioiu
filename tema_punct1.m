22b3e2321930fe1ac5dd2526a1056ac38b67eea5
qazcomyqaz <32986730+qazcomyqaz@users.noreply.github.com>
2017-11-08 23:10:29 +0200

    Add files via upload

---
+++ tema_punct1.m
@@ -0,0 +1,20 @@
+function tema_punct1()
+
+fs = 5000;
+t = 0:1/fs:100;
+
+x2 = square(pi*t,25);
+
+for i = 1:1:length(x2)
+   if x2(i) > 0
+       x2(i) = x2(i)/2;
+   end
+end
+
+plot(t,x2)
+axis([0 5.2 -1.2 1.2])
+xlabel('Time (sec)')
+ylabel('Amplitude')
+title('Semnal dreptunghiular, factor de umplere 25%')
+
+end
